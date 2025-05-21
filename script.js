5197c524781e6d5428819407cdcd8bb1780c7bdb
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-21 20:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+  const getSumBtn = document.createElement("button");
+  getSumBtn.append("Get Total Price");
+  document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-    const priceList=document.querySelectorAll(".price");
-    let total=0;
-    for(let i=0;i<priceList.length;i++){
-        total+=Number(priceList[i].innerHTML)
-    }
-    const lastElemet=document.createElement("tr");
-    lastElemet.innerHTML=`
-        <td class="final" colSpan="2">${total}</td>`
-    document.getElementsByTagName("tbody")[0].appendChild(lastElemet);
-};
+  const getSum = () => {
+  //Add your code here
+      const ans=document.getElementById("ans");
+      if(ans){
+        return;
+      } 
+      const priceList=document.querySelectorAll(".price");
+      let total=0;
+      for(let i=0;i<priceList.length;i++){
+          total+=Number(priceList[i].innerHTML)
+      }
+      const lastElemet=document.createElement("tr");
+      lastElemet.innerHTML=`
+          <td colspan="2" id="ans">${total}</td>`
+      document.getElementsByTagName("tbody")[0].appendChild(lastElemet);
+  };
 
-getSumBtn.addEventListener("click", getSum);
+  getSumBtn.addEventListener("click", getSum);
